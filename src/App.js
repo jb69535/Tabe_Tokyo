cfb0e997f9ed75bd8fae38b2e05013abc2347dd5
jb69535 <92282867+jb69535@users.noreply.github.com>
2023-05-24 16:11:33 -0400

    Components, Pages, Styles, Setups

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Map from './Components/Card/Map';
+import Left_Sidebar from './Components/Sidebar/Left_Sidebar';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Signin from './Pages/Signin'
+import Signup from './Pages/Signup';
+import React, {useState} from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    </>
   );
 }
-
-export default App;
