243862a40ef61053194b1ad181dff06228c47160
jb69535 <92282867+jb69535@users.noreply.github.com>
2024-01-09 00:09:42 -0500

    tabetokyo revise

---
+++ src/firebase.ts
@@ -0,0 +1,11 @@
+// firebase.ts
+import firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  // Your Firebase config object
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
