dc776d7d83e7310940c34ec644ccdac8321ffda4
jb69535 <92282867+jb69535@users.noreply.github.com>
2024-01-17 21:21:52 -0500

    Trying to fix deploy error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import logo from './assets/logo.svg';
+//import logo from './assets/logo.svg';
 import './styles/App.css';
 import Login from "./components/Authentication/Login";
 import Header from "./components/Layout/Header";
